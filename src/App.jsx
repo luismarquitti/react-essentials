3e97249bc0afd2305713253c2e976c261caabdfe
Luis Marquitti <luismarquitti@gmail.com>
2025-04-12 11:29:11 -0300

    Refactor App component to use CoreConcepts and Examples components; add new Section and Tabs components

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
-import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  function handleSelect() {
-    console.log('Button Selected!')
-  }
   return (
-    <div>
-      <Header />
-      <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
-          </menu>
-        </section>
-      </main>
-    </div>
+      <>
+        <Header />
+        <main>
+          <CoreConcepts />
+          <Examples />
+        </main>
+      </>
   );
 }
 
